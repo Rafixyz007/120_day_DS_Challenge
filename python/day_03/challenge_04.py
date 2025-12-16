275125a6470483cc5a4547577c4f17f17cd73d97
Rafixyz007 <122726297+Rafixyz007@users.noreply.github.com>
2025-12-16 21:11:26 +0600

    Day 03 challenge completed

---
+++ python/day_03/challenge_04.py
@@ -0,0 +1,12 @@
+## 4. Micro challenge: The String Walker
+
+## Goal: create a string word = "DATA". write a loop that prints every letter on a new line.
+
+
+string = "DATA"
+
+for item in string:
+    print(item)
+
+
+
